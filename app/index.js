e917fc53dab43978b6320f7059c8192c26d87fb9
karl <karlkivisild@gmail.com>
2024-10-02 13:56:32 +0300

    Create Splash screen

---
+++ app/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { SafeAreaView } from 'react-native'
+import Splash from './(auth)/Splash'
+
+const App = () => {
+    return (
+        <SafeAreaView>
+            <Splash />
+        </SafeAreaView>
+    )
+}
+
+export default App
