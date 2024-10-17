494d346f28a4d015fcd862a23dc56cf67493db10
karl <karlkivisild@gmail.com>
2024-10-17 11:16:45 +0300

    Create homepage screen

---
+++ components/CategoryBox/styles.js
@@ -0,0 +1,26 @@
+import { StyleSheet } from "react-native-web";
+import { colors } from "../../utils/colors";
+
+export const styles = StyleSheet.create({
+    container: {
+        marginHorizontal: 8,
+        marginBottom: 8,
+        marginTop: 8,
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    title: {
+        color: colors.grey
+    },
+    image: {
+        width: 32,
+        height: 32
+    },
+    imageContainer: {
+        backgroundColor: colors.lightGray,
+        padding: 8,
+        borderRadius: 8,
+        marginBottom: 8
+    }
+})
